be1e51683cd2c98458c873c046342cc1e9a5dc2f
Tainna <tainnaandryelli@gmail.com>
2023-12-12 10:06:32 -0300

    config projeto e add template padrao

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+<!-- chama 'template' de dentro da pasta 'templates' -->
+@extends('templates.template')
+
+@section('content')
+  <h1 class="text-center">Atendimento ao Contribuinte</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+/*O PHP com o Laravel utiliza um controle de rotas para exibir as páginas para o usuário*/
+
+/*Route::get('/teste', function () {
+    return view('welcome');
+});*/
+
+//passando rota através de um controller chamado 'BookController' e vai chamar o método index de dentro desse controller:
+use App\Http\Controllers\BookController;
+Route::resource('/', BookController::class);
